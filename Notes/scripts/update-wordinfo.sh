8d1d4fe0fe2661ad1a358c36c4d503a604e7e604
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2020-03-07 15:10:15 +0100

    Translate comments into English for clarity

--- Notes/scripts/update-wordinfo.sh
+++ Notes/scripts/update-wordinfo.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 ########################################
-# Fonctions
+# Functions
 ########################################
-# Elles permettent surtout de rendre le script plus lisible
+# Mainly to make the script more readable
 
-# Supprime BOM, commentaires et lignes vides du flux
+# Removes BOMs, comments and blank lines from the stream
 clean() { sed -e 's/\xEF\xBB\xBF/\n/' | grep -av '^//' | grep -avE '^[[:space:]]*$' ; }
 
-# Supprime du flux les lignes présentes dans le fichier passé en 1er argument
+# Removes the file lines passed as the first parameter from the stream
 exclude() { grep -avxFf "$1" ; }
 
-# Ne conserve du flux que les lignes présentes dans le fichier passé en 1er argument
+# Keeps only the file lines passed as the first parameter in the stream
 intersect() { grep -axFf "$1" ; }
 
-# Supprime les doublons du flux
+# Removes duplicates from the stream
 unique() { sort --unique ; }
 
-# Extrait du flux le contenu des tags utilisés par le système de traduction
+# Extract the element's content used by the translation system from the stream
 extract_tag_content() {
   grep -aE '\.(label|labelMale|labelMalePlural|labelFemale|labelFemalePlural|pawnSingular|pawnsPlural)>' \
   | sed 's/^.*>\([^<]*\)<.*$/\1/' ;
@@ -25,44 +25,44 @@ extract_tag_content() {
 extract_tag_male_content() { grep -aE '\.(labelMale|labelMalePlural)>' | sed 's/^.*>\([^<]*\)<.*$/\1/' ; }
 extract_tag_female_content() { grep -aE '\.(labelFemale|labelFemalePlural)>' | sed 's/^.*>\([^<]*\)<.*$/\1/' ; }
 
-# Passe tout en minuscule
+# Passes all in lowercase
 to_lowercase() { PERLIO=:utf8 perl -ne 'print lc $_' ;  }
 
 ########################################
-# Début du script
+# Start of the script
 ########################################
 set -ex
 
-# Va à la racine du projet
+# Go to the root of the project
 cd $(dirname $(readlink -f $0))/../..
 
-# Créer un répertoire de travail et s'assure qu'il soit supprimé à la fin
+# Create a temporary folder and make sure it is deleted at the end
 WORKDIR=$(mktemp -d)
 trap "rm -rf $WORKDIR" EXIT
 
-# Force la langue pour les outils qui en tiennent compte
+# Force the language for tools that take it into account
 export LANG=fr_FR.UTF-8 LC_ALL=fr_FR.UTF-8
 
-# Liste des tags provenant du XML
+# List of all words coming from XML
 cat */DefInjected/{PawnKind,Faction,Thing,WorldObject}Def/*.xml | extract_tag_content | to_lowercase | unique > $WORKDIR/all
 
-# Ajouter labelMale* dans WordInfo/Gender/Male.txt
+# Add labelMale* into WordInfo/Gender/Male.txt
 cat */WordInfo/Gender/Male.txt > $WORKDIR/all_males.txt
 cat */DefInjected/{PawnKind,Faction,Thing,WorldObject}Def/*.xml | extract_tag_male_content >> $WORKDIR/all_males.txt
 cat $WORKDIR/all_males.txt | to_lowercase | unique > Core/WordInfo/Gender/Male.txt
 
-# Ajouter labelFemale* dans WordInfo/Gender/Female.txt
+# Add labelFemale* into WordInfo/Gender/Female.txt
 cat */WordInfo/Gender/Female.txt > $WORKDIR/all_females.txt
 cat */DefInjected/{PawnKind,Faction,Thing,WorldObject}Def/*.xml | extract_tag_female_content >> $WORKDIR/all_females.txt
 cat $WORKDIR/all_females.txt | to_lowercase | unique > Core/WordInfo/Gender/Female.txt
 
-# Liste des mots déjà classés
+# List of words already classified
 cat Core/WordInfo/Gender/{Male,Female}.txt | unique > $WORKDIR/wordinfo
 
-# Ajoute les nouveaux mots dans WordInfo/Gender/new_words.txt
+# Add new words into WordInfo/Gender/new_words.txt
 exclude $WORKDIR/wordinfo < $WORKDIR/all | unique > Core/WordInfo/Gender/new_words.txt
 
-# Supprime les mots obsolètes des fichiers WordInfo/Gender/{Male,Female}.txt
+# Removes obsolete words from WordInfo/Gender/{Male,Female}.txt files
 for GENDER in Male Female; do
   intersect $WORKDIR/all < Core/WordInfo/Gender/$GENDER.txt > $WORKDIR/$GENDER.txt
   mv $WORKDIR/$GENDER.txt Core/WordInfo/Gender/$GENDER.txt
