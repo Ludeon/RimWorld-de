1a3f7f03667ef5a68b0ed5e09d4f5b5b4ad0be8b
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2024-11-04 23:22:19 +0100

    Separate WordInfo by DLC That is, use WordInfo in each DLC folder, not just in Core. Facilitates future translations of new DLCs.

--- update-wordinfo.ps1
+++ update-wordinfo.ps1
@@ -2,86 +2,101 @@
 
 $PSDefaultParameterValues["*:Encoding"] = "UTF8"
 
-# Create a temporary folder
-$temp = New-Item "$env:temp\$([GUID]::NewGuid())" -ItemType "Directory"
-
-# Create WordInfo/Gender folder
-$main = New-Item "Core/WordInfo/Gender" -ItemType "Directory" -Force
+# Get DLC folders (including Core)
+$dlcs = Get-ChildItem -Directory | Where-Object { Test-Path -Path (Join-Path -Path $_.FullName -ChildPath "DefInjected") }
 
 # Paths of the XML files in which the words should be searched
 $paths = @(
-"*\DefInjected\BackstoryDef"
-"*\DefInjected\BodyDef"
-"*\DefInjected\BodyPartDef"
-"*\DefInjected\ChemicalDef"
-"*\DefInjected\FactionDef"
-"*\DefInjected\GameConditionDef"
-"*\DefInjected\HediffDef"
-"*\DefInjected\PawnKindDef"
-"*\DefInjected\RoyalTitleDef"
-"*\DefInjected\TerrainDef"
-"*\DefInjected\ThingDef"
-"*\DefInjected\TraderKindDef"
-"*\DefInjected\WorldObjectDef"
-"*\DefInjected\RitualBehaviorDef"
-"*\DefInjected\PsychicRitualRoleDef"
+"DefInjected\BackstoryDef"
+"DefInjected\BodyDef"
+"DefInjected\BodyPartDef"
+"DefInjected\ChemicalDef"
+"DefInjected\FactionDef"
+"DefInjected\GameConditionDef"
+"DefInjected\HediffDef"
+"DefInjected\PawnKindDef"
+"DefInjected\RoyalTitleDef"
+"DefInjected\TerrainDef"
+"DefInjected\ThingDef"
+"DefInjected\TraderKindDef"
+"DefInjected\WorldObjectDef"
+"DefInjected\RitualBehaviorDef"
+"DefInjected\PsychicRitualRoleDef"
 )
 
-# Search words in the XML files
-foreach ($path in $paths)
+# Enumerate DLC folders
+foreach ($dlc in $dlcs)
 {
-  Get-Content -Path "$path/*" -Filter "*.xml" | Select-String -Pattern "<(((?!\bstages\b).)*(\.label|\.labelNoLocation|\.pawnSingular|title|titleFemale|\.chargeNoun|\.customLabel|\.labelMale|\.labelFemale))>(.*?)</\1>" -All | ForEach-Object { $_.matches.groups[4].value.toLower() } >> "$temp/all.txt"
-}
+  # Create a temporary folder
+  $temp = New-Item "$env:temp\$([GUID]::NewGuid())" -ItemType "Directory"
 
-# Sort the list of all found words
-Get-Content "$temp/all.txt" | Sort-Object -Unique | Set-Content "$temp/all.txt"
+  # Create WordInfo\Gender folder
+  $main = New-Item "$dlc\WordInfo\Gender" -ItemType "Directory" -Force
 
-# Create files
-foreach ($fileName in "Male", "Female", "Neuter", "Other", "new_words")
-{
-  if (!(test-path "$main/$fileName.txt"))
+  # Search words in the XML files
+  foreach ($path in $paths)
   {
-    New-Item -Path $main -Name "$fileName.txt"
+    if (!(test-path "$dlc\$path"))
+    {
+      continue
+    }
+    Get-Content -Path "$dlc\$path\*" -Filter "*.xml" | Select-String -Pattern "<(((?!\bstages\b).)*(\.label|\.labelNoLocation|\.pawnSingular|title|titleFemale|\.chargeNoun|\.customLabel|\.labelMale|\.labelFemale))>(.*?)</\1>" -All | ForEach-Object { $_.matches.groups[4].value.toLower() } >> "$temp\all.txt"
   }
-}
 
-# Sort the list of male words
-Get-Content "$main/Male.txt" | Sort-Object -Unique | Set-Content "$main/Male.txt"
+  # Sort the list of all found words
+  Get-Content "$temp\all.txt" | Sort-Object -Unique | Set-Content "$temp\all.txt"
 
-# Sort the list of female words
-Get-Content "$main/Female.txt" | Sort-Object -Unique | Set-Content "$main/Female.txt"
+  # Create files
+  foreach ($fileName in "Male", "Female", "Neuter", "Other", "new_words")
+  {
+    if (!(test-path "$main\$fileName.txt"))
+    {
+      New-Item -Path $main -Name "$fileName.txt"
+    }
+  }
 
-# Sort the list of neuter words
-Get-Content "$main/Neuter.txt" | Sort-Object -Unique | Set-Content "$main/Neuter.txt"
+  # Sort the list of male words
+  Get-Content "$main\Male.txt" | Sort-Object -Unique | Set-Content "$main\Male.txt"
 
-# Sort the list of other words
-Get-Content "$main/Other.txt" | Sort-Object -Unique | Set-Content "$main/Other.txt"
+  # Sort the list of female words
+  Get-Content "$main\Female.txt" | Sort-Object -Unique | Set-Content "$main\Female.txt"
 
-# Save a list of words already classified
-Get-Content (Get-ChildItem -Path "$main/*" -Include "Male.txt", "Female.txt", "Neuter.txt", "Other.txt") | Sort-Object -Unique | Set-Content "$temp/wordinfo.txt"
+  # Sort the list of neuter words
+  Get-Content "$main\Neuter.txt" | Sort-Object -Unique | Set-Content "$main\Neuter.txt"
 
-# Save a list of words not classified
-$objects = @{
-  ReferenceObject  = (Get-Content -Path "$temp/wordinfo.txt")
-  DifferenceObject = (Get-Content -Path "$temp/all.txt")
-}
-if ($objects.ReferenceObject -and $objects.DifferenceObject)
-{
-  Compare-Object @objects -PassThru | Where-Object { $_.SideIndicator -eq "=>" } > "$main/new_words.txt"
-}
+  # Sort the list of other words
+  Get-Content "$main\Other.txt" | Sort-Object -Unique | Set-Content "$main\Other.txt"
 
-# Removes obsolete words from the lists
-foreach ($gender in "Male", "Female", "Neuter", "Other")
-{
+  # Save a list of words already classified
+  Get-Content (Get-ChildItem -Path "$main\*" -Include "Male.txt", "Female.txt", "Neuter.txt", "Other.txt") | Sort-Object -Unique | Set-Content (New-Item -Path $temp -Name "wordinfo.txt")
+
+  # Save a list of words not classified
   $objects = @{
-    ReferenceObject  = (Get-Content -Path "$temp/all.txt")
-    DifferenceObject = (Get-Content -Path "$main/$gender.txt")
+    ReferenceObject  = (Get-Content -Path "$temp\wordinfo.txt")
+    DifferenceObject = (Get-Content -Path "$temp\all.txt")
   }
   if ($objects.ReferenceObject -and $objects.DifferenceObject)
   {
-    Compare-Object @objects -IncludeEqual -ExcludeDifferent -PassThru > "$main/$gender.txt"
+    Compare-Object @objects -PassThru | Where-Object { $_.SideIndicator -eq "=>" } > "$main\new_words.txt"
   }
-}
+  else
+  {
+    Set-Content -Path "$main\new_words.txt" -Value $objects.DifferenceObject
+  }
 
-# Delete the temporary folder
-Remove-Item -Recurse $temp
+  # Removes obsolete words from the lists
+  foreach ($gender in "Male", "Female", "Neuter", "Other")
+  {
+    $objects = @{
+      ReferenceObject  = (Get-Content -Path "$temp\all.txt")
+      DifferenceObject = (Get-Content -Path "$main\$gender.txt")
+    }
+    if ($objects.ReferenceObject -and $objects.DifferenceObject)
+    {
+      Compare-Object @objects -IncludeEqual -ExcludeDifferent -PassThru > "$main\$gender.txt"
+    }
+  }
+
+  # Delete the temporary folder
+  Remove-Item -Recurse $temp
+}
