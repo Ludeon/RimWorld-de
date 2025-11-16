ec54063a11d10175a09a7d4cbcd24b7154bd0932
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2025-11-16 14:51:43 +0100

    Improve translations

--- update-wordinfo.ps1
+++ update-wordinfo.ps1
@@ -1,5 +1,5 @@
 # Search for all scripts in the current directory that start with "update-wordinfo-" and end with ".ps1"
-$scripts = Get-ChildItem -Filter "update-wordinfo-*.ps1"
+$scripts = Get-ChildItem -Filter "update-wordinfo-*.ps1" | Sort-Object Name
 
 # Run each found script sequentially
 foreach ($script in $scripts) {
